2eea785824ab19a87ad8ccb41fafeb6b29c0d0cc
George Melikov <mail@gmelikov.ru>
2025-03-04 10:40:20 +0300

    Initial version

---
+++ genesis/images/install.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# Copyright 2025 Genesis Corporation
+#
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+set -eu
+set -x
+set -o pipefail
+
+[[ "$EUID" == 0 ]] || exec sudo -s "$0" "$@"
+
+export EXTERNAL_URL="http://localhost"
+
+apt-get update
+apt-get install -y curl openssh-server ca-certificates tzdata perl
+# Configure some SMTP, or install postfix by hand
+# apt install -y postfix
+
+curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.deb.sh | bash
+
+apt-get update
+apt-get install gitlab-ee -y
+# gitlab-ctl reconfigure
+
+
+# cat /etc/gitlab/initial_root_password
+# WARNING: This value is valid only in the following conditions
+#          1. If provided manually (either via `GITLAB_ROOT_PASSWORD` environment variable or via `gitlab_rails['initial_root_password']` setting in `gitlab.rb`, it was provided before database was seeded for the first time (usually, the first reconfigure run).
+#          2. Password hasn't been changed manually, either via UI or via command line.
+#
+#          If the password shown here doesn't work, you must reset the admin password following https://docs.gitlab.com/ee/security/reset_user_password.html#reset-your-root-password.
+
+# Password: XXX
+
+# NOTE: This file will be automatically deleted in the first reconfigure run after 24 hours.
